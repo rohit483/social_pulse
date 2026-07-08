83b08974c42b1215f8dfec027dcc455a2709762e
ROHIT <rohitchawda4241@gmail.com>
2026-07-08 17:52:00 +0530

    fix: email

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2026 ROHIT CHAWDA
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import logging
 
@@ -89,6 +104,14 @@ def privacy_policy():
 @app.route('/terms')
 def terms():
     return render_template('terms.html')
+
+@app.route('/license')
+def license_page():
+    try:
+        with open('LICENSE', 'r') as f:
+            return Response(f.read(), mimetype='text/plain')
+    except Exception as e:
+        return jsonify({"error": "License file not found."}), 404
 
 #-----------------------------------------------------------------------------------
 # --- 2. Sentiment Analysis Routes ---
